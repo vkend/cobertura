e0ae87744a3f25ee2f651976e52dc6e1d6302642
Vishal Kendre <vishal.kendre@gmail.com>
2021-06-22 20:23:35 -0400

    Using jaxb to create xml report

--- cobertura/src/main/java/net/sourceforge/cobertura/reporting/xml/XMLReport.java
+++ cobertura/src/main/java/net/sourceforge/cobertura/reporting/xml/XMLReport.java
@@ -25,20 +25,50 @@
 
 package net.sourceforge.cobertura.reporting.xml;
 
-import net.sourceforge.cobertura.coveragedata.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.stream.XMLOutputFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.sourceforge.cobertura.coveragedata.ClassData;
+import net.sourceforge.cobertura.coveragedata.CoverageData;
+import net.sourceforge.cobertura.coveragedata.JumpData;
+import net.sourceforge.cobertura.coveragedata.LineData;
+import net.sourceforge.cobertura.coveragedata.PackageData;
+import net.sourceforge.cobertura.coveragedata.ProjectData;
+import net.sourceforge.cobertura.coveragedata.SourceFileData;
+import net.sourceforge.cobertura.coveragedata.SwitchData;
 import net.sourceforge.cobertura.reporting.ComplexityCalculator;
+import net.sourceforge.cobertura.reporting.xml.elements.Class;
+import net.sourceforge.cobertura.reporting.xml.elements.Classes;
+import net.sourceforge.cobertura.reporting.xml.elements.Condition;
+import net.sourceforge.cobertura.reporting.xml.elements.Conditions;
+import net.sourceforge.cobertura.reporting.xml.elements.Coverage;
+import net.sourceforge.cobertura.reporting.xml.elements.Line;
+import net.sourceforge.cobertura.reporting.xml.elements.Lines;
+import net.sourceforge.cobertura.reporting.xml.elements.Method;
+import net.sourceforge.cobertura.reporting.xml.elements.Methods;
+import net.sourceforge.cobertura.reporting.xml.elements.Package;
+import net.sourceforge.cobertura.reporting.xml.elements.Packages;
+import net.sourceforge.cobertura.reporting.xml.elements.Source;
+import net.sourceforge.cobertura.reporting.xml.elements.Sources;
 import net.sourceforge.cobertura.util.FileFinder;
 import net.sourceforge.cobertura.util.Header;
 import net.sourceforge.cobertura.util.IOUtil;
 import net.sourceforge.cobertura.util.StringUtil;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class XMLReport {
 
@@ -47,54 +77,189 @@ public class XMLReport {
 
 	protected final static String coverageDTD = "coverage-04.dtd";
 
-	private final PrintWriter pw;
-	private final FileFinder finder;
 	private final ComplexityCalculator complexity;
 	private int indent = 0;
 
 	public XMLReport(ProjectData projectData, File destinationDir,
-			FileFinder finder, ComplexityCalculator complexity)
-			throws IOException {
+			FileFinder finder, ComplexityCalculator complexity) throws JAXBException, IOException{
 		this.complexity = complexity;
-		this.finder = finder;
 
 		File file = new File(destinationDir, "coverage.xml");
-		pw = IOUtil.getPrintWriter(file);
+		XMLOutputFactory factory = XMLOutputFactory.newFactory();
 
-		try {
-			println("<?xml version=\"1.0\"?>");
-			println("<!DOCTYPE coverage SYSTEM \"http://cobertura.sourceforge.net/xml/"
-					+ coverageDTD + "\">");
-			println("");
-
-			double ccn = complexity.getCCNForProject(projectData);
+		try (PrintWriter printWriter = IOUtil.getPrintWriter(file)){
+			double ccn = this.complexity.getCCNForProject(projectData);
 			int numLinesCovered = projectData.getNumberOfCoveredLines();
 			int numLinesValid = projectData.getNumberOfValidLines();
 			int numBranchesCovered = projectData.getNumberOfCoveredBranches();
 			int numBranchesValid = projectData.getNumberOfValidBranches();
 
-			// TODO: Set a schema?
-			//println("<coverage " + sourceDirectories.toString() + " xmlns=\"http://cobertura.sourceforge.net\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://cobertura.sourceforge.net/xml/coverage.xsd\">");
-			println("<coverage line-rate=\""
-					+ projectData.getLineCoverageRate() + "\" branch-rate=\""
-					+ projectData.getBranchCoverageRate()
-					+ "\" lines-covered=\"" + numLinesCovered
-					+ "\" lines-valid=\"" + numLinesValid
-					+ "\" branches-covered=\"" + numBranchesCovered
-					+ "\" branches-valid=\"" + numBranchesValid
+			Coverage coverage = new Coverage();
+			coverage.setLineRate(String.valueOf(projectData.getLineCoverageRate()));
+			coverage.setBranchRate(String.valueOf(projectData.getBranchCoverageRate()));
+			coverage.setLinesCovered(String.valueOf(numLinesCovered));
+			coverage.setLinesValid(String.valueOf(numLinesValid));
+			coverage.setBranchesCovered(String.valueOf(numBranchesCovered));
+			coverage.setBranchesValid(String.valueOf(numBranchesValid));
+			coverage.setComplexity(String.valueOf(ccn));
+			coverage.setVersion(String.valueOf(Header.version()));
+			coverage.setTimestamp(String.valueOf(new Date().getTime()));
+			
+			Sources sources = new Sources();
+			for (Iterator it = finder.getSourceDirectoryList().iterator(); it
+					.hasNext();) {
+				String dir = (String) it.next();
+				Source source = new Source();
+				source.setContent(dir);
+				sources.getSource().add(source);
+			}
+			coverage.setSources(sources);
+			
+			Packages packages = new Packages();
+			Iterator it = projectData.getPackages().iterator();
+			while (it.hasNext()) {
+				PackageData packageData = (PackageData) it.next();
+				Package pkg = new Package();
+				packages.getPackage().add(pkg);
+				pkg.setName(packageData.getName());
+				pkg.setLineRate(String.valueOf(packageData.getLineCoverageRate()));
+				pkg.setBranchRate(String.valueOf(packageData.getBranchCoverageRate()));
+				pkg.setComplexity(String.valueOf(this.complexity.getCCNForPackage(packageData)));
+				
+				Classes classes = new Classes();
+				pkg.setClasses(classes);
+				
+				Iterator it2 = packageData.getSourceFiles().iterator();
+				while (it2.hasNext()) {
+					SourceFileData sourceFileData = (SourceFileData) it2.next();
+					Iterator it3 = sourceFileData.getClasses().iterator();
+					while (it3.hasNext()) {
+						ClassData classData = (ClassData) it3.next();
+						dumpClasses(classes, classData);
+					}
+				}
+				
+			}
 
-					+ "\" complexity=\"" + ccn
+			coverage.setPackages(packages);
+			
+	        JAXBContext jaxbContext = JAXBContext.newInstance(Coverage.class);
+	        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+	        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+	        jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
+	        printWriter.write("<?xml version=\"1.0\"?>"
+	        		  + "\n<!DOCTYPE coverage SYSTEM \"http://cobertura.sourceforge.net/xml/"
+    				  + coverageDTD + "\">\n");
+	        jaxbMarshaller.marshal(coverage, printWriter);
 
-					+ "\" version=\"" + Header.version() + "\" timestamp=\""
-					+ new Date().getTime() + "\">");
+		} 
+	}
 
-			increaseIndentation();
-			dumpSources();
-			dumpPackages(projectData);
-			decreaseIndentation();
-			println("</coverage>");
-		} finally {
-			pw.close();
+	private void dumpClasses(Classes classes, ClassData classData) {
+		Class clazz = new Class();
+		classes.getClazz().add(clazz);
+		
+		clazz.setName(String.valueOf(classData.getName()));
+		clazz.setFilename(String.valueOf(classData.getSourceFileName()));
+		clazz.setLineRate(String.valueOf(classData.getLineCoverageRate()));
+		clazz.setBranchRate(String.valueOf(classData.getBranchCoverageRate()));
+		clazz.setComplexity(String.valueOf(this.complexity.getCCNForClass(classData)));
+		
+		Methods methods = new Methods();
+		clazz.setMethods(methods);
+
+		dumpMethods(classData, methods);
+		
+		SortedSet<CoverageData> lines = classData.getLines();
+		Lines clazzLines = new Lines();
+		clazz.setLines(clazzLines);
+		dumpLines(lines, clazzLines);
+	}
+
+	private void dumpMethods(ClassData classData, Methods methods) {
+		SortedSet<String> sortedMethods = new TreeSet<>();
+		sortedMethods.addAll(classData.getMethodNamesAndDescriptors());
+		Iterator<String> iter = sortedMethods.iterator();
+		while (iter.hasNext()) {
+			Method method = new Method();
+			String nameAndSig = (String) iter.next();
+			methods.getMethod().add(method);
+
+			dumpMethod(classData, nameAndSig, method);
+
+		}
+	}
+
+	private void dumpMethod(ClassData classData, String nameAndSig, Method method) {
+		String name = nameAndSig.substring(0, nameAndSig.indexOf('('));
+		String signature = nameAndSig.substring(nameAndSig.indexOf('('));
+		double lineRate = classData.getLineCoverageRate(nameAndSig);
+		double branchRate = classData.getBranchCoverageRate(nameAndSig);
+		int methodComplexity = this.complexity.getCCNForMethod(classData, name, signature);
+		
+		method.setName(xmlEscape(name));
+		method.setSignature(xmlEscape(signature));
+		method.setLineRate(String.valueOf(lineRate));
+		method.setBranchRate(String.valueOf(branchRate));
+		method.setComplexity(String.valueOf(methodComplexity));
+
+		increaseIndentation();
+		Collection<CoverageData> lines = classData.getLines(nameAndSig);
+		Lines methodLines = new Lines();
+		method.setLines(methodLines);
+		
+		dumpLines(lines, methodLines);
+	}
+
+	private void dumpLines(Collection<CoverageData> lines, Lines xmlLines) {
+		SortedSet<CoverageData> sortedLines = new TreeSet<>();
+		sortedLines.addAll(lines);
+		Iterator<CoverageData> iter4 = sortedLines.iterator();
+		while (iter4.hasNext()) {
+			LineData lineData = (LineData) iter4.next();
+			Line line = new Line();
+			xmlLines.getLine().add(line);
+			dumpLine(lineData, line);
+		}
+	}
+
+	private void dumpLine(LineData lineData, Line line) {
+		int lineNumber = lineData.getLineNumber();
+		long hitCount = lineData.getHits();
+		boolean hasBranch = lineData.hasBranch();
+		String conditionCoverage = lineData.getConditionCoverage();
+		
+		line.setNumber(String.valueOf(lineNumber));
+		line.setBranch(String.valueOf(hasBranch));
+		line.setHits(String.valueOf(hitCount));
+
+		line.setConditionCoverage(String.valueOf(conditionCoverage));
+		List<Conditions> conditionList = line.getConditions();
+		dumpConditions(lineData, conditionList);
+	}
+
+	private void dumpConditions(LineData lineData, List<Conditions> conditionList) {
+		Conditions conditions = new Conditions();
+		conditionList.add(conditions);
+		for (int i = 0; i < lineData.getConditionSize(); i++) {
+			String coverage = lineData.getConditionCoverage(i);
+			Object conditionData = lineData.getConditionData(i);
+			Condition condition = new Condition();
+			conditions.getCondition().add(condition);
+			dumpCondition(coverage, conditionData, condition);
+		}
+	}
+
+	private void dumpCondition(String coverage, Object conditionData, Condition condition) {
+		if (conditionData instanceof JumpData) {
+			JumpData jumpData = (JumpData) conditionData;
+			condition.setCoverage(coverage);
+			condition.setNumber(String.valueOf(jumpData.getConditionNumber()));
+		} else {
+			SwitchData switchData = (SwitchData) conditionData;
+			condition.setCoverage(coverage);
+			condition.setNumber(String.valueOf(switchData.getSwitchNumber()));
+			condition.setType("switch");
 		}
 	}
 
@@ -107,207 +272,11 @@ public class XMLReport {
 			indent--;
 	}
 
-	void indent() {
-		for (int i = 0; i < indent; i++) {
-			pw.print("\t");
-		}
-	}
-
-	void println(String ln) {
-		indent();
-		pw.println(ln);
-	}
-
-	private void dumpSources() {
-		println("<sources>");
-		increaseIndentation();
-		for (Iterator it = finder.getSourceDirectoryList().iterator(); it
-				.hasNext();) {
-			String dir = (String) it.next();
-			dumpSource(dir);
-		}
-		decreaseIndentation();
-		println("</sources>");
-	}
-
-	private void dumpSource(String sourceDirectory) {
-		println("<source>" + sourceDirectory + "</source>");
-	}
-
-	private void dumpPackages(ProjectData projectData) {
-		println("<packages>");
-		increaseIndentation();
-
-		Iterator it = projectData.getPackages().iterator();
-		while (it.hasNext()) {
-			dumpPackage((PackageData) it.next());
-		}
-
-		decreaseIndentation();
-		println("</packages>");
-	}
-
-	private void dumpPackage(PackageData packageData) {
-		logger.debug("Dumping package " + packageData.getName());
-
-		println("<package name=\"" + packageData.getName() + "\" line-rate=\""
-				+ packageData.getLineCoverageRate() + "\" branch-rate=\""
-				+ packageData.getBranchCoverageRate() + "\" complexity=\""
-				+ complexity.getCCNForPackage(packageData) + "\"" + ">");
-		increaseIndentation();
-		dumpSourceFiles(packageData);
-		decreaseIndentation();
-		println("</package>");
-	}
-
-	private void dumpSourceFiles(PackageData packageData) {
-		println("<classes>");
-		increaseIndentation();
-
-		Iterator it = packageData.getSourceFiles().iterator();
-		while (it.hasNext()) {
-			dumpClasses((SourceFileData) it.next());
-		}
-
-		decreaseIndentation();
-		println("</classes>");
-	}
-
-	private void dumpClasses(SourceFileData sourceFileData) {
-		Iterator it = sourceFileData.getClasses().iterator();
-		while (it.hasNext()) {
-			dumpClass((ClassData) it.next());
-		}
-	}
-
-	private void dumpClass(ClassData classData) {
-		logger.debug("Dumping class " + classData.getName());
-
-		println("<class name=\"" + classData.getName() + "\" filename=\""
-				+ classData.getSourceFileName() + "\" line-rate=\""
-				+ classData.getLineCoverageRate() + "\" branch-rate=\""
-				+ classData.getBranchCoverageRate() + "\" complexity=\""
-				+ complexity.getCCNForClass(classData) + "\"" + ">");
-		increaseIndentation();
-
-		dumpMethods(classData);
-		dumpLines(classData);
-
-		decreaseIndentation();
-		println("</class>");
-	}
-
-	private void dumpMethods(ClassData classData) {
-		println("<methods>");
-		increaseIndentation();
-
-		SortedSet sortedMethods = new TreeSet();
-		sortedMethods.addAll(classData.getMethodNamesAndDescriptors());
-		Iterator iter = sortedMethods.iterator();
-		while (iter.hasNext()) {
-			dumpMethod(classData, (String) iter.next());
-		}
-
-		decreaseIndentation();
-		println("</methods>");
-	}
-
-	private void dumpMethod(ClassData classData, String nameAndSig) {
-		String name = nameAndSig.substring(0, nameAndSig.indexOf('('));
-		String signature = nameAndSig.substring(nameAndSig.indexOf('('));
-		double lineRate = classData.getLineCoverageRate(nameAndSig);
-		double branchRate = classData.getBranchCoverageRate(nameAndSig);
-		int methodComplexity = complexity.getCCNForMethod(classData, name, signature);
-
-		println("<method name=\"" + xmlEscape(name) + "\" signature=\""
-				+ xmlEscape(signature) + "\" line-rate=\"" + lineRate
-		    		+ "\" branch-rate=\"" + branchRate + "\" complexity=\"" + methodComplexity + "\">");
-		increaseIndentation();
-		dumpLines(classData, nameAndSig);
-		decreaseIndentation();
-		println("</method>");
-	}
-
 	private static String xmlEscape(String str) {
 		str = StringUtil.replaceAll(str, "<", "&lt;");
 		str = StringUtil.replaceAll(str, ">", "&gt;");
 		return str;
 	}
 
-	private void dumpLines(ClassData classData) {
-		dumpLines(classData.getLines());
-	}
-
-	private void dumpLines(ClassData classData, String methodNameAndSig) {
-		dumpLines(classData.getLines(methodNameAndSig));
-	}
-
-	private void dumpLines(Collection lines) {
-		println("<lines>");
-		increaseIndentation();
-
-		SortedSet sortedLines = new TreeSet();
-		sortedLines.addAll(lines);
-		Iterator iter = sortedLines.iterator();
-		while (iter.hasNext()) {
-			dumpLine((LineData) iter.next());
-		}
-
-		decreaseIndentation();
-		println("</lines>");
-	}
-
-	private void dumpLine(LineData lineData) {
-		int lineNumber = lineData.getLineNumber();
-		long hitCount = lineData.getHits();
-		boolean hasBranch = lineData.hasBranch();
-		String conditionCoverage = lineData.getConditionCoverage();
-
-		String lineInfo = "<line number=\"" + lineNumber + "\" hits=\""
-				+ hitCount + "\" branch=\"" + hasBranch + "\"";
-		if (hasBranch) {
-			println(lineInfo + " condition-coverage=\"" + conditionCoverage
-					+ "\">");
-			dumpConditions(lineData);
-			println("</line>");
-		} else {
-			println(lineInfo + "/>");
-		}
-	}
-
-	private void dumpConditions(LineData lineData) {
-		increaseIndentation();
-		println("<conditions>");
-
-		for (int i = 0; i < lineData.getConditionSize(); i++) {
-			Object conditionData = lineData.getConditionData(i);
-			String coverage = lineData.getConditionCoverage(i);
-			dumpCondition(conditionData, coverage);
-		}
-
-		println("</conditions>");
-		decreaseIndentation();
-	}
-
-	private void dumpCondition(Object conditionData, String coverage) {
-		increaseIndentation();
-		StringBuffer buffer = new StringBuffer("<condition");
-		if (conditionData instanceof JumpData) {
-			JumpData jumpData = (JumpData) conditionData;
-			buffer.append(" number=\"").append(jumpData.getConditionNumber())
-					.append("\"");
-			buffer.append(" type=\"").append("jump").append("\"");
-			buffer.append(" coverage=\"").append(coverage).append("\"");
-		} else {
-			SwitchData switchData = (SwitchData) conditionData;
-			buffer.append(" number=\"").append(switchData.getSwitchNumber())
-					.append("\"");
-			buffer.append(" type=\"").append("switch").append("\"");
-			buffer.append(" coverage=\"").append(coverage).append("\"");
-		}
-		buffer.append("/>");
-		println(buffer.toString());
-		decreaseIndentation();
-	}
 
 }
